8810454f36a9e74107335414b2f3edf2e610e37d
Diego Gomez <diegocor4@gmail.com>
2024-10-15 11:16:32 -0300

    correccion de mozo

--- mozo.py
+++ mozo.py
@@ -1,33 +1,42 @@
+from pizza import Pizza
+
 class Mozo:
 
+    # Constructor
     def __init__(self, nom: str):
-        self.__nombre = nom
-        self.__pizzas = []
+        self.__nombre = nom # Atributo de instancia: nombre de tipo string
+        self.__pizzas = [] # Atributo de instancia: lista de objetos Pizza
 
+    # Comando: establece el nombre del mozo
     def establecerNombre(self, nom: str):
         self.__nombre = nom
 
-    def tomarPizzas(self, pizzas):
+    # Comando: toma pizzas
+    def tomarPizzas(self, pizzas: list[Pizza]):
         pizzasTomadas = len(self.__pizzas)
         pizzasATomar = len(pizzas)
-        if (pizzasATomar - pizzasTomadas) < 0:
+        if (pizzasATomar + pizzasTomadas) > 2:
             print(self.__nombre + ": El mozo puede tomar un máximo de 2 pizzas!")
         else:
             for pizza in pizzas:
                 print(self.__nombre + ": tomando una de " + pizza.obtenerVariedad() + " para ser entregada")
                 self.__pizzas.append(pizza)
     
+    # comando: sirve las pizzas que ha tomado
     def servirPizzas(self):
         for pizza in self.__pizzas:
             print(self.__nombre + ": Sirviendo pizza de " + pizza.obtenerVariedad())
-        self.__pizzas = []
-
+        self.__pizzas = [] # Vacia la lista de pizzas una vez servidas
+        
+    # Consulta: obtiene el nombre del mozo
     def obtenerNombre(self):
         return self.__nombre
     
+    # Consulta: obtiene la lista de pizzas
     def obtenerPizzas(self):
         return self.__pizzas
     
+    # consulta: devuelve si el mozo puede tomar mas pizzas
     def obtenerEstadoLibre(self):
         pizzasTomadas = len(self.__pizzas)
         return pizzasTomadas < 2
---
+++ testerpizzeria.py
@@ -0,0 +1,68 @@
+
+from maestropizzero import MaestroPizzero
+from mozo import Mozo
+from pizza import Pizza
+from pizzavariedad import PizzaVariedad
+from orden import Orden
+
+class TesterPizzeria:
+    def __init__(self):
+        self.pizzero = MaestroPizzero("Carlos")
+        
+    def ejecutar(self):
+        # Crear variedades de pizza
+        variedadMuzzarella = PizzaVariedad("Muzzarella", 800.0)
+        variedadNapolitana = PizzaVariedad("Napolitana", 950.5)
+        variedadFugazzeta = PizzaVariedad("Fugazzeta", 900.0)
+        
+        # Crear pizzas usando eas variedades
+        pizza1 = Pizza(variedadMuzzarella)
+        pizza2 = Pizza(variedadNapolitana)
+        pizza3 = Pizza(variedadFugazzeta)
+        
+        
+        # Crear una orden co las pizzas
+        orden1 = Orden(1, [pizza1, pizza2, pizza3])
+        
+        # Cocinar las pizzas
+        print("\n Cocinando las pizzas...")
+        self.pizzero.cocinar()
+        self.imprimirEstadoOrden(orden1)
+        
+        # Entregar las pizzas
+        print("\nEntregando las pizzas...")
+        pizzasEntregadas = self.pizzero.entregar(orden1)
+        self.imprimirEstadoOrden(orden1)
+        
+        print("\nPizzas entregadas: ")
+        for pizza in pizzasEntregadas:
+            print(f"- {pizza.obtenerVariedad().obtenerNombreVariedad()}")
+            
+    def imprimirEstadoOrden(self, orden: Orden):
+        estado_orden = orden.obtenerEStadoOrden()
+        estado_str = ""
+        if estado_orden == Orden.ESTADO_PARA_ENTREGAR:
+            estado_str = "Penddiente de cocinar"
+        elif estado_orden == Orden.ESTADO_PARA_ENTREGAR:
+            estado_str = "Lista para entregar"
+        elif estado_orden == Orden.ESTADO_ENTREGADA:
+            estado_str = "Entregada"
+            
+        print(f"Orden {orden.obtenerEstadoOrden()}: {estado_str}")
+        print("Pizzas en la orden:")
+        for pizza in orden.obtenerEstadoOrden():
+            estado_pizza = pizza.obtenerEstado()
+            estado_pizza_str = ""
+            if estado_pizza == Pizza.ESTADO_POR_COCINAR:
+                estado_pizza_str = "Por cocinar"
+            elif estado_pizza == Pizza.ESTADO_COCINADA:
+                estado_pizza_str = "cocinada"
+            elif estado_pizza == Pizza.ESTADO_ENTREGADA:
+                estado_pizza_str = "Entregada"
+                
+            print(f"- {pizza.obtenerVaridad().obtenerNombreVaridad()}: {estado_pizza_str}")
+                
+# ejecutar el TestPizzeria
+if __name__ == '__main__':
+    tester = TesterPizzeria()
+    tester.ejecutar()        
